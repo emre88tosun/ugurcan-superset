8bf12f4be565c41d4dc7a232b467c2fdc84af595
Emre Tosun <emre.tosun@leanscale.com>
2022-11-27 11:28:20 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { embedDashboard } from "@superset-ui/embedded-sdk";
+import { useEffect } from "react";
+import "./index.css";
+import { Buffer } from "buffer";
+window.Buffer = window.Buffer || Buffer;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const promise = new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(
+				"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImZpcnN0X25hbWUiOiJNeUFwcCBVc2VyIiwibGFzdF9uYW1lIjoiTXlBcHAgVXNlciIsInVzZXJuYW1lIjoibXlBcHBVc2VyIn0sInJlc291cmNlcyI6W3sidHlwZSI6ImRhc2hib2FyZCIsImlkIjoiZDQxZTJjNDItNTVjZS00YTgwLWEwYWQtMjdkY2FmNzU1NWM3In1dLCJybHNfcnVsZXMiOlt7ImNsYXVzZSI6IndlYnNpdGVfaWQ9MTUifV0sImlhdCI6MTY2NjYyMjA5OS4zOTYzMDA2LCJleHAiOjE2NjY2MjIzOTkuMzk2MzAwNiwiYXVkIjoiaHR0cDovL3N1cGVyc2V0OjgwODgvIiwidHlwZSI6Imd1ZXN0In0.1OCaFERmaKQNf-5vJDeGHBIJqBqn9BC8V6Y5gkS9Ukg"
+			);
+		}, 2000);
+	});
+
+	useEffect(() => {
+		embedDashboard({
+			id: "d41e2c42-55ce-4a80-a0ad-27dcaf7555c7",
+			supersetDomain: "https://superset.dcafood.com",
+			mountPoint: document.getElementById("myContainer"), // html element in which iframe render
+			fetchGuestToken: () => promise,
+			dashboardUiConfig: { hideTitle: true },
+		});
+	}, [promise]);
+
+	return <div className="h-screen w-screen bg-red-500" id="myContainer"></div>;
+};
 
 export default App;
